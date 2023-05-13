dffbf7d730c4ca80f6b3cfeeb766b615cc970cfa
Robert Vicente <robertvicente32@gmail.com>
2023-05-13 10:09:19 -0300

    primer grabacion

---
+++ src/productController.js
@@ -0,0 +1,21 @@
+const ProductManager = require("../productManager")
+const productManager = new ProductManager("./src/products.json")
+const productController = {
+    index: function (req, res) {
+        if(req.query.limit){
+            let limit = Number(req.query.limit)
+            const products = productManager.getProducts(limit)
+            return res.json(products)
+        }else{
+            const products = productManager.getProducts()
+            return res.json(products)
+        }
+    },
+    show: function(req, res) {
+        let pid = Number(req.params.pid)
+        const product = productManager.getProductById(pid)
+        return res.json(product)
+    }
+}
+
+module.exports = productController
